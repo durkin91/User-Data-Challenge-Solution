53f41e54c3289151126da94c6f12fbe4944292c5
Nikki Durkin <nikki@99dresses.com>
2014-09-12 22:34:56 +1000

    Initial Commit

---
+++ User Data Challenge Solution/NDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NDAppDelegate.h
+//  User Data Challenge Solution
+//
+//  Created by Nikki Durkin on 9/12/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ User Data Challenge Solution/NDViewController.m
@@ -0,0 +1,29 @@
+//
+//  NDViewController.m
+//  User Data Challenge Solution
+//
+//  Created by Nikki Durkin on 9/12/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "NDViewController.h"
+
+@interface NDViewController ()
+
+@end
+
+@implementation NDViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
